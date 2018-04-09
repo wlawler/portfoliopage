9ccb5835b0bc6a0c6d3313b13fd32a245e25ca33
wes lawler <wes.lawler2@gmail.com>
2018-04-09 16:50:53 -0500

    beginning stage of portfolio page

--- portpage/src/app/app.module.ts
+++ portpage/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
