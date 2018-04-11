4345f2c39e3a3e0af9420967909b7715719f1af1
wes lawler <wes.lawler2@gmail.com>
2018-04-11 17:34:57 -0500

    contact component added

--- portpage/src/app/app.module.ts
+++ portpage/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule
