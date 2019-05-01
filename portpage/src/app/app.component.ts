adbe8b1d060e8894b37abc398ec2d71ddddfcb01
wes lawler <wes.lawler2@gmail.com>
2019-05-01 13:38:32 -0400

    deleted fire icon from contact me

--- portpage/src/app/app.component.ts
+++ portpage/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
+  isIn  = false;   // store state
+  
+  toggleState() { // click handler
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false;
+  }
+  
 }
